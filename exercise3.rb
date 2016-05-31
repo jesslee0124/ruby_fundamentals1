5f8072736066b551387b54ba78308d9f5ef2376a
YOUJIN LEE <jess.lee0124@gmail.com>
2016-05-31 18:08:00 -0400

    Add age bit

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 name = gets.chomp
-puts "Hi #{name}!"
+puts "Hi #{name}! How old are you?"
+age = gets.chomp.to_i
+yearBorn = 2016 - age
+puts "You were born in #{yearBorn}!"
